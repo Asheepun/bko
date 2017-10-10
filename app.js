3c686d77eb29ff0739e5f56c10ec38f42be47a46
Asheepun <pussagussa@gmail.com>
2017-10-10 23:03:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socketController = require("./socketController.js");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
